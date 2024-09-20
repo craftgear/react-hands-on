7548cd6b8c020c10b7e7f843a5a054deb1eac58f
Shunsuke Watanabe <craftgear@gmail.com>
2024-09-20 14:10:52 +0900

    delete all

--- src/pages/NotFound.tsx
+++
@@ -1,1 +0,0 @@
-export const NotFound = () => <h1 className="text-3xl">Not Found</h1>
--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-import daisyui from 'daisyui';
-export default {
-  content: ['./index.html', './src/**/*.{jsx,tsx,js,html}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [daisyui],
-};
