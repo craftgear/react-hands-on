3538f99c16254562353d2c9429ab809583aeb279
Shunsuke Watanabe <craftgear@gmail.com>
2024-12-19 16:28:35 +0900

    ch7

--- src/pages/Todos.tsx
+++ src/pages/Todos.tsx
@@ -1,7 +1,39 @@
+import { ChangeEventHandler } from "react";
+import { useGetTodos, usePatchTodo } from "../api/todos";
+
 export const Todos = () => {
+  const { data, isPending, error } = useGetTodos();
+
+  const { mutate } = usePatchTodo();
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const id = e.target.value;
+    mutate({ id, done: e.target.checked });
+  };
+
+  if (isPending) {
+    return <div>Loading ...</div>;
+  }
+
+  if (error) {
+    return <div>Error</div>;
+  }
+
   return (
     <div>
       <h1>Todos</h1>
+      {data?.map(({ id, title, done }) => (
+        <div key={id}>
+          <input
+            type="checkbox"
+            id={`${id}`}
+            value={id}
+            checked={done ? true : false}
+            onChange={handleChange}
+          />
+          <label htmlFor={`${id}`}>{title}</label>
+        </div>
+      ))}
     </div>
   );
 };
