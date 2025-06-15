1a32f610f9f461770e699882580041d2dfc5f54c
Shunsuke Watanabe <craftgear@gmail.com>
2025-06-15 12:54:53 +0900

    clean slate

--- playwright/index.tsx
+++
@@ -1,4 +0,0 @@
-// Import styles, initialize component theme here.
-// import '../src/common.css';
-import '../src/index.css';
-import '../src/App.css';
--- src/components/ViteIcon.tsx
+++
@@ -1,9 +0,0 @@
-import viteLogo from '/vite.svg'
-
-export const ViteIcon = () => {
-  return (
-    <a href="https://vitejs.dev" target="_blank">
-      <img src={viteLogo} className="logo" alt="Vite logo" />
-    </a>
-  )
-}
